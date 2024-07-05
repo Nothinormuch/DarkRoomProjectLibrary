31936f7d03ac711ff3d3222b917d1b073ce7a2b7
AX/f~&q,D0?qIOQ/yVsI+Tw?q <nikhilsharma2006.all.on@gmail.com>
2024-07-05 07:42:56 +0530

    IvR calling Stage 1

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-print("Test1")
-print("Test2")
-print("Test3")
+import cv2
+cam = cv2.VideoCapture(0)
+imagecount=1
+while True:
+    frame = cam.read()[1]
+    frame[0:280,0:260] =  frame[100:380 , 200:460]
+    cv2.imshow("Image",frame)
+    if cv2.waitKey(1) == 13:
+        break
+    elif cv2.waitKey(1) == ord("q"):
+        cv2.imwrite("./Images/Image{}.png".format(imagecount),frame)
+        imagecount+=1
+cv2.destroyAllWindows()
+cam.release()
